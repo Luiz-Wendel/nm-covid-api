f15ef846a3d6b0f0c99a3ec6bc5c6678454e9425
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-08-02 17:12:52 -0300

    Getting data from query string

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,12 +3,32 @@ const axios = require('axios');
 
 const homeController = {
   index: async (req, res) => {
+    // Extract date interval and state from query
+    const { state, dateStart, dateEnd } = req.query;
+
     try {
-      const response = await axios.get(
-        'https://brasil.io/api/dataset/covid19/caso/data/?state=PR&date=2020-05-10'
+      if (!state) return res.status(400).json({ error: 'State not defined!' });
+
+      if (!dateStart || !dateEnd)
+        return res.status(400).json({
+          error:
+            'Date interval not defined! Make sure to set both DateStart and DateEnd!',
+        });
+
+      const responseStart = await axios.get(
+        `https://brasil.io/api/dataset/covid19/caso/data/?state=${state}&date=${dateStart}`
       );
 
-      return res.json(response.data);
+      const responseEnd = await axios.get(
+        `https://brasil.io/api/dataset/covid19/caso/data/?state=${state}&date=${dateEnd}`
+      );
+
+      const startList = responseStart.data.results;
+      const endList = responseEnd.data.results;
+
+      return res.json(
+        `${JSON.stringify(startList)}\n\n\n${JSON.stringify(endList)}`
+      );
     } catch (error) {
       debug(error);
       return res.json(error);
