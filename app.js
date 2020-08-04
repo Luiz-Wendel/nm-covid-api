df1e28dac1c70a7b01082e8e9e3278b69f52b5bc
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-08-04 12:21:40 -0300

    Adding cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const cors = require('cors');
 
 // Routers
 const homeRouter = require('./routes/homeRouter');
@@ -9,6 +10,9 @@ const homeRouter = require('./routes/homeRouter');
 const port = process.env.SERVER_PORT || 3000;
 
 const app = express();
+
+// Allow outside requests
+app.use(cors());
 
 // Print simplified requests on terminal
 app.use(morgan('tiny'));
